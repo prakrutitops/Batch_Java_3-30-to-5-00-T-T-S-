9ee2cb4822c8bec23b71d89169f9af761875807a
prakrutitops <109515514+prakrutitops@users.noreply.github.com>
2022-07-28 16:32:14 +0530

    Add files via upload

---
+++ Module1/28-7/ThrowEx.java
@@ -0,0 +1,21 @@
+package com.a287;
+
+public class ThrowEx 
+{
+	static void validate(int age)
+	{
+		if(age>18)
+		{
+			System.out.println("welcome to vote");
+		}
+		else
+		{
+			throw new ArithmeticException("Not Valid");
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		validate(15);
+	}
+}
