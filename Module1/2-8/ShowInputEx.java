9810caefe3502bd6634df538622fa918915083c2
prakrutitops <109515514+prakrutitops@users.noreply.github.com>
2022-08-02 16:31:44 +0530

    Add files via upload

---
+++ Module1/2-8/ShowInputEx.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class ShowInputEx 
+{
+	public static void main(String[] args) {
+		
+		String ans=JOptionPane.showInputDialog("Enter your name");
+		System.out.println(ans);
+		
+		
+	}
+}
