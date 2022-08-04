c52bd7d5b96194ee2086a9404e1e35b339dc88cf
prakrutitops <109515514+prakrutitops@users.noreply.github.com>
2022-08-04 16:58:02 +0530

    Add files via upload

---
+++ Module1/4-8/IconEx.java
@@ -0,0 +1,26 @@
+import java.awt.Frame;
+import java.awt.Image;
+import java.awt.Toolkit;
+
+public class IconEx 
+{	
+	
+	Frame frame;
+	
+	public IconEx() 
+	{
+		// TODO Auto-generated constructor stub
+		frame=new Frame();
+		Image icon = Toolkit.getDefaultToolkit().getImage("E:\\a.png");    
+		frame.setIconImage(icon);
+		frame.setLayout(null);     
+		frame.setSize(400,400);     
+		frame.setVisible(true);     
+	}
+	public static void main(String[] args) {
+		
+		
+		new IconEx();
+	}
+	
+}
