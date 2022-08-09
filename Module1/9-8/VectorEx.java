00ac9920bfdb25d344c082a1061c30c9f33965eb
prakrutitops <109515514+prakrutitops@users.noreply.github.com>
2022-08-09 16:41:14 +0530

    Add files via upload

---
+++ Module1/9-8/VectorEx.java
@@ -0,0 +1,37 @@
+import java.util.Vector;
+
+public class VectorEx 
+{
+	public static void main(String[] args) 
+	{
+		
+		Vector v =new Vector<>(5);
+		
+		v.add("a");
+		v.add("b");
+		v.add("c");
+		v.add("a");
+		v.addElement("d");
+		v.addElement("e");
+		v.add("c");
+		v.add("a");
+		v.addElement("d");
+		v.addElement("e");
+		v.addElement("e");
+		
+		System.out.println(v.size());
+		System.out.println(v.capacity());
+		
+		if(v.contains("t"))
+		{
+			System.out.println("called");
+		}
+		else
+		{
+			System.out.println("Not Called");
+		}
+		
+		System.out.println(v.firstElement());
+		System.out.println(v.lastElement());
+	}
+}
