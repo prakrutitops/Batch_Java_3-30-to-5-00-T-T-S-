d1325ca9459b50af5d305e49c50064e67a0df2c2
prakrutitops <109515514+prakrutitops@users.noreply.github.com>
2022-08-13 16:30:07 +0530

    Add files via upload

---
+++ Module1/13-8/Abc.java
@@ -0,0 +1,34 @@
+package com.a138;
+
+
+
+enum student2
+{
+	John(11), Bella(10), Sam(13), Viraaj(9);
+	
+	int age;
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	private student2(int age)
+	{
+		// TODO Auto-generated constructor stub
+		this.age=age;
+	}
+	
+	
+	
+}
+public class Abc 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println(student2.Bella.age);
+	}
+}
